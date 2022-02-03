759afa4c3d08fbf185b5650489008910ad7374b6
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2022-02-03 12:45:56 +0100

    removed hardcoded movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -8,64 +9,27 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-          movies: [
-            {"Title":"Encanto",
-            "Genre":{"Name":"Musical"},
-            "Director":{"Name":"Byron Howar",
-            "Birth":"1968-01-01"},
-            "ImagePath":"https://www.imdb.com/title/tt2953050/mediaviewer/rm2541025281/"
-        },
-            {"Title":"Spider-Man: No Way Home",
-            "Genre":{"Name":"Action"},
-            "Director":{"Name":"Jon Watts"},
-            "ImagePath":"https://www.imdb.com/title/tt10872600/mediaviewer/rm3936939521/"
-        },
-            {"Title":"Shang-chi and the legend of the ten rings",
-            "Genre":{"name":"Action"},
-            "Director":{"name":"Destin daniel cretton"},
-            "ImagePath":"https://www.imdb.com/title/tt9376612/mediaviewer/rm4214145793/"
-        },
-            {"Title":"Eternal",
-            "Genre":{"Name":"Action"},
-            "Director":{"Name":"Kevin Feige"},
-            "ImagePath":"https://www.imdb.com/title/tt9032400/mediaviewer/rm1427628801/"
-        },
-            {"Title":"Last Christmas",
-            "Genre":{"Name":"Comedy"},
-            "Director":{"Name":"Paul Feig"},
-            "ImagePath":"https://www.imdb.com/title/tt8623904/mediaviewer/rm1963759361/"
-        },
-            {"Title":"Doctor Strange",
-            "Genre":{"Name":"Action"},
-            "Director":{"Name":"Scott Derrickson"},
-            "ImagePath":"https://www.imdb.com/title/tt1211837/mediaviewer/rm3012758016/"
-        },
-            {"Title":"Doctor Strange 2",
-            "Genre":{"Name":"Action"},
-            "Director":{"Name":"Sam Raimi"},
-            "ImagePath":"https://www.imdb.com/title/tt9419884/mediaviewer/rm1100344577/"
-        },
-            {"Title":"Zootopia",
-            "Genre":{"Name":"Animated"},
-            "Director":{"Name":"Byron Howard"},
-            "Birthday":["1968-01-01"],
-            "ImagePath":"https://www.imdb.com/title/tt2948356/mediaviewer/rm2690413824/"
-        },
-            {"Title":"The Witches",
-            "Genre":{"Name":"Fantasy"},
-            "Director":{"Name":"Robert Zemeckis"},
-            "ImagePath":"https://www.imdb.com/title/tt0805647/mediaviewer/rm1215865857/"
-        }
-         ],
+          movies: [],
           selectedMovie: null
-        };
-    }
+        }
+      }
+        componentDidMount() {
+          axios.get('https://myflixapp1.herokuapp.com/movies')
+          .then(response => {
+            this.setState({
+              movies: response.data
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
         setSelectedMovie(newSelectedMovie) {
             this.setState({
               selectedMovie: newSelectedMovie
             });
-          }
-          render() {
+        }
+          render(){
             const { movies, selectedMovie } = this.state;
         
             
@@ -82,5 +46,4 @@ export class MainView extends React.Component {
               </div>
             );
           }
-        
         }
